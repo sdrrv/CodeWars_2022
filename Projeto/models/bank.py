d1f6813dabb7a67574d041894b8bc0958b23e997
sdrrv <duartecelvas@gmail.com>
2022-03-12 15:05:20 +0000

    Bank update

--- Projeto/models/bank.py
+++ Projeto/models/bank.py
@@ -1,3 +1,11 @@
+from datetime import date
+from models import *
 class Bank:
     def __init__(self):
-        pass
+        self.date = date(2022,1,1)
+        self.users = []
+    
+    def getDate(self):
+        return date
+    def increaseDate(self):
+        pass 
