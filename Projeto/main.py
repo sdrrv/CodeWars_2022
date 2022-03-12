c89a29ce29e9de3889fd5e8c62e9c70446842bc9
sdrrv <duartecelvas@gmail.com>
2022-03-12 16:41:17 +0000

    Main updated

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,10 +1,12 @@
 from views.terminalView import terminal
 from views.website import create_app
+from models.bank import Bank
 
 
 #app = create_app()
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    terminal()
+    bank = Bank()    
+    terminal(bank)
  
