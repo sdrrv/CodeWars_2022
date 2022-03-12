28a0e000d8b3a6628e33d230a3e6298e0f94929d
Doginainar <nun0dend4s@gmail.com>
2022-03-12 14:22:50 +0000

    Conta

---
+++ Projeto/models/conta.py
@@ -0,0 +1,14 @@
+class Account:
+    def __init__(self,balance, account_id, location):
+        self.balance = balance
+        self.account_id = account_id
+        self.location = location
+
+
+class Premium_account(Account):
+    
+
+
+
+
+
