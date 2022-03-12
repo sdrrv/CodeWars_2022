b3520a7f729835843a18d34375cbaa34bdfb2bbf
galbatorix2011 <afonsoalmeida.b@gmail.com>
2022-03-12 09:24:28 +0000

    Clion first project added

---
+++ prob1Clion/cmake-build-debug/CMakeFiles/prob1Clion.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/afonsoalmeida/IST/codeWars/CodeWars_2022/prob1Clion/main.cpp" "/Users/afonsoalmeida/IST/codeWars/CodeWars_2022/prob1Clion/cmake-build-debug/CMakeFiles/prob1Clion.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ prob1Clion/cmake-build-debug/CMakeFiles/prob1Clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prob1Clion.dir/main.cpp.o"
+  "prob1Clion"
+  "prob1Clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prob1Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
