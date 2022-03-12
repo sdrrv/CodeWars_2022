199ea9fc718ff5a44472b97b8476221e6228a118
ALexandre <alexandre.corte@tecnico.ulisboa.pt>
2022-03-12 16:41:22 +0000

    Alteracoes

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,10 +1,12 @@
 from views.terminalView import terminal
-from views.website import create_app
+#from views.website import create_app
+from models import bank
 
 
 #app = create_app()
 
 if __name__ == '__main__':
     #app.run(debug=True)
+
     terminal()
  
